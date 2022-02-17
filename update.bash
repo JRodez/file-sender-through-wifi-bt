d6de6a5f0ff4b6b1dd52d76a4a164205da939c35
Jérémie Rodez <jeremierodez@outlook.com>
2022-02-17 23:49:59 +0100

    Update update.bash

--- update.bash
+++ update.bash
@@ -4,5 +4,8 @@ sudo rm -r ./file-sender-through-wifi-bt/
 git clone https://github.com/JRodez/file-sender-through-wifi-bt.git
 sleep 1
 ls
-cd ./file-sender-through-wifi-bt/
-cd ../file-sender-through-wifi-bt 
+cd file-sender-through-wifi-bt
+# cd ../file-sender-through-wifi-bt 
+echo OK
+
+
