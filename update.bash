5f7c713fa00c2099de776286427773e0e5a8508e
Jérémie Rodez <jeremierodez@outlook.com>
2022-02-17 23:40:44 +0100

    Update update.bash

--- update.bash
+++ update.bash
@@ -1,2 +1,5 @@
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
 sudo rm -r ./file-sender-through-wifi-bt/
 git clone https://github.com/JRodez/file-sender-through-wifi-bt.git
+cd file-sender-through-wifi-bt
