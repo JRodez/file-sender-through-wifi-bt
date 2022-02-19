bd340bd6dd68fb9736f548181f922a2a88e82cde
Jérémie Rodez <jeremierodez@outlook.com>
2022-02-19 21:03:04 +0100

    d

--- client.sh
+++ client.sh
@@ -3,6 +3,6 @@ wpa_cli terminate
 sleep 1
 ifconfig wlan0 down
 iwconfig wlan0 mode Ad-Hoc
-ifconfig wlan0 essid KAJE
+iwconfig wlan0 essid KAJE
 ifconfig wlan0 192.168.2.2
 ifconfig wlan0 up
--- server.sh
+++ server.sh
@@ -1,6 +1,6 @@
 sudo rfkill unblock wifi; sudo rfkill unblock all
 ifconfig wlan0 down
 iwconfig wlan0 mode Ad-Hoc
-ifconfig wlan0 essid KAJE
+iwconfig wlan0 essid KAJE
 ifconfig wlan0 192.168.2.1
 ifconfig wlan0 up
