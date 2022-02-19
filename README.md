577c255a45857a7218badf1f2a0f520c7b458bdd
Jérémie Rodez <jeremierodez@outlook.com>
2022-02-19 21:25:51 +0100

    update

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# file-sender-through-wifi-bt
+# Send files over WIFI and Bluetooth
+
+## Set wlan0 in AD-HOC mode :
+USAGE : adhoc.sh <network name> <node number>
+
+## Send files
+
+## Receive files
+
+
+Copyright MOUSSAOUI Karim and RODEZ Jérémie
--- adhoc.sh
+++ adhoc.sh
@@ -3,7 +3,7 @@ wpa_cli terminate
 sleep 1
 ifconfig wlan0 down
 iwconfig wlan0 mode Ad-Hoc
-iwconfig wlan0 essid KAJE
-echo 192.168.2.$1
-ifconfig wlan0 192.168.2.$1
+iwconfig wlan0 essid $1
+echo My address is 192.168.2.$2 over $1 network.
+ifconfig wlan0 192.168.2.$2
 ifconfig wlan0 up
