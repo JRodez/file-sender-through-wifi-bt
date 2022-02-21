b5c747f2e6db921b186f0dd5167ea467d710e9b4
Jérémie Rodez <jeremierodez@outlook.com>
2022-02-21 16:49:16 +0100

    Revert "Update sender.py"
    This reverts commit 7177d9b21dde40c20d18429f19797e567348a32a.

--- sender.py
+++ sender.py
@@ -1,17 +1,44 @@
-# coding: utf-8
-
 import socket
+import tqdm
 import os
+import sys
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("", 1111))
+SEPARATOR = "<SEPARATOR>"
+BUFFER_SIZE = 4096 # send 4096 bytes each time step
 
-print("Le nom du fichier que vous voulez récupérer:")
-file_name = input(">> ") # utilisez raw_input() pour les anciennes versions python
-s.send(file_name.encode())
-file_name = '%s' % (file_name,)
-r = s.recv(9999999)
-with open(file_name,'wb') as _file:
-    _file.write(r)
-print("Le fichier a été correctement copié dans : %s." % file_name)
-os.system('python3 ' + file_name)
+# the ip address or hostname of the server, the receiver
+host = sys.argv[1] #"192.168.1.101"
+# the port, let's use 5001
+port = int(sys.argv[2])
+
+# the name of file we want to send, make sure it exists
+filename = sys.argv[3]
+# get the file size
+filesize = os.path.getsize(filename)
+
+# create the client socket
+s = socket.socket()
+
+print(f"[+] Connecting to {host}:{port}")
+s.connect((host, port))
+print("[+] Connected.")
+
+# send the filename and filesize
+s.send(f"{filename}{SEPARATOR}{filesize}".encode())
+
+# start sending the file
+progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+with open(filename, "rb") as f:
+    while True:
+        # read the bytes from the file
+        bytes_read = f.read(BUFFER_SIZE)
+        if not bytes_read:
+            # file transmitting is done
+            break
+        # we use sendall to assure transimission in
+        # busy networks
+        s.sendall(bytes_read)
+        # update the progress bar
+        progress.update(len(bytes_read))
+# close the socket
+s.close()
