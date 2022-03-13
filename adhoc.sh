b6ffc03ec12572b9d60ba1ef3d7500e7fe0381e4
Jérémie Rodez <jeremierodez@outlook.com>
2022-03-13 16:52:12 +0100

    d

--- adhoc.sh
+++ adhoc.sh
@@ -6,13 +6,14 @@ then
 	exit
 fi
 
+ifconfig wlan0 up
+CHANNEL=$(echo $scan | cut -d '(' -f 2 | cut -d ' ' -f 2 | cut -d ')' -f 1 | head -n 1)
+echo "The least used channel is channel $CHANNEL."
 rfkill unblock wifi &> /dev/null ; rfkill unblock all &> /dev/null
 wpa_cli terminate &> /dev/null
 sleep 1
 scan="$(sudo iwlist wlan0 scan | grep Frequency | sort | uniq -c | sort -n )"
 echo "$scan"
-CHANNEL=$(echo $scan | cut -d '(' -f 2 | cut -d ' ' -f 2 | cut -d ')' -f 1 | head -n 1)
-echo "The least used channel is channel $CHANNEL."
 ifconfig wlan0 down
 iwconfig wlan0 mode Ad-Hoc
 iwconfig wlan0 channel $CHANNEL
