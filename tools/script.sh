895df5df99db6167963950de177d533d67dab561
Jérémie Rodez <jeremierodez@outlook.com>
2022-03-13 16:10:47 +0100

    r

--- tools/script.sh
+++
@@ -1,2 +0,0 @@
-sudo iwconfig wlan0 mode ad-hoc channel 11 essid $1 
-sudo ip addr add 192.168.1.$2 dev wlan0
--- tools/server.sh
+++
@@ -1,6 +0,0 @@
-sudo rfkill unblock wifi; sudo rfkill unblock all
-ifconfig wlan0 down
-iwconfig wlan0 mode Ad-Hoc
-iwconfig wlan0 essid KAJE
-ifconfig wlan0 192.168.2.1
-ifconfig wlan0 up
