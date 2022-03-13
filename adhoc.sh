d99a9ac82e7edd869cd0c962f7367a596f0d74a5
Jérémie Rodez <jeremierodez@outlook.com>
2022-03-13 16:35:14 +0100

    Update adhoc.sh

--- adhoc.sh
+++ adhoc.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+@Echo off
 
 if [ $# -ne 2 ]
 then
@@ -6,18 +7,18 @@ then
 	exit
 fi
 
-rfkill unblock wifi; rfkill unblock all
-wpa_cli terminate
+rfkill unblock wifi &> /dev/null ; rfkill unblock all &> /dev/null
+wpa_cli terminate &> /dev/null
 sleep 1
 scan="$(sudo iwlist wlan0 scan | grep Frequency | sort | uniq -c | sort -n )"
 echo "$scan"
 CHANNEL=$(echo $scan | cut -d '(' -f 2 | cut -d ' ' -f 2 | cut -d ')' -f 1 | head -n 1)
-echo The channel the less used is the Channel $CHANNEL.
+echo "The least used channel is channel $CHANNEL."
 ifconfig wlan0 down
 iwconfig wlan0 mode Ad-Hoc
 iwconfig wlan0 channel $CHANNEL
 iwconfig wlan0 essid $1
-echo "My ip address is 192.168.2.$2 over $1 network using the $CHANNEL channel."
+echo "My ip address is 192.168.2.$2 over $1 network using the channel $CHANNEL."
 ifconfig wlan0 192.168.2.$2
 ifconfig wlan0 up
 exit
